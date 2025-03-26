8332240d3c6a2e45ce3129f4095955183f084718
Roberto Mejía <59416763+robermejia@users.noreply.github.com>
2025-03-25 22:31:08 -0500

    avance_1

--- semana_6/Tarea6/js/index.js
+++
@@ -1,7 +0,0 @@
-function startLoading() {
-    const loadingBar = document.getElementById('loadingBar');
-    loadingBar.style.width = '100%';
-    setTimeout(() => {
-        loadingBar.style.width = '5%'; 
-    }, 1500);
-}
